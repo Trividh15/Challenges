1e8f9dc119b5d2e8f86a8d9cc85e5a74070a18e7
Devansh Mandloi <131014087+Trividh15@users.noreply.github.com>
2024-01-13 21:21:22 +0530

    Create AnagramStringProblem.cpp

---
+++ LeetCode/AnagramStringProblem.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int minSteps(const string& s, const string& t) {
+        if (s.size() != t.size())
+        {    
+            return -1;
+        }
+
+        int charfreqfors[26] = {0};
+
+        for (char ch : s) {
+            if ('a' <= ch && ch <= 'z') {
+                charfreqfors[ch - 'a']++;
+            }
+        }
+
+        int charfreqfort[26] = {0};
+
+        for (char ch : t) {
+            if ('a' <= ch && ch <= 'z') {
+                charfreqfort[ch - 'a']++;
+            }
+        }
+
+        int steps = 0;
+
+        for (int i = 0; i < 26; i++) {
+            steps += abs(charfreqfors[i] - charfreqfort[i]);
+        }
+
+        return steps / 2; // Each difference counts twice (s to t and t to s)
+    }
+};
+
