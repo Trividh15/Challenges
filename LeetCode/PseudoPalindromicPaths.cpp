f79495358d18364b62c61079a5771e4610928f68
Devansh Mandloi <131014087+Trividh15@users.noreply.github.com>
2024-01-24 14:49:45 +0530

    Create PseudoPalindromicPaths.cpp

---
+++ LeetCode/PseudoPalindromicPaths.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int pseudoPalindromicPaths(TreeNode* root) {
+        return dfs(root, 0);
+    }
+
+private:
+    int dfs(TreeNode* node, int pathCount) {
+        if (!node) {
+            return 0;
+        }
+
+        pathCount ^= (1 << node->val);  
+        if (!node->left && !node->right) {
+           
+            return (pathCount & (pathCount - 1)) == 0 ? 1 : 0;
+        }
+
+        int leftCount = dfs(node->left, pathCount);
+        int rightCount = dfs(node->right, pathCount);
+
+        return leftCount + rightCount;
+    }
+};
